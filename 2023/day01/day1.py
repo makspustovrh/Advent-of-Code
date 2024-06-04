be28627c03e419f1dc0863f65e677f9f3251742b
theonlyrealmax <max.crypto@hotmail.com>
2024-06-04 20:55:23 +0200

    added comments to day1

--- 2023/day01/day1.py
+++ 2023/day01/day1.py
@@ -1,17 +1,17 @@
 def calibration(line):
-    res = []
+    res = [] # var for all numbers in line
     for i in line:
-        if i.isnumeric():
+        if i.isnumeric(): # find all numbers
             res.append(int(i))
-    if len(res) < 2:
+    if len(res) < 2: # if only 1 number in line
         return res[0]*10 + res[0]
     return res[0]*10 + res[-1]
 
 if __name__ == "__main__":
-    sum = 0
-    input_file = "input.txt"
+    sum = 0 # var for sum of calibration numbers
+    input_file = "input.txt" # puzzle input
     with open(input_file, 'r') as f:
         lines = f.readlines()
-    for line in lines:
-        sum += calibration(str(line))
+    for line in lines: # for each line
+        sum += calibration(str(line)) # add the calibration number
     print(sum)
